cbaddd6289f4699bbba25c6a1d70c4a58d589485
Ruddy Rodriguez <ruddy.rodriguez.r@hotmail.com>
2023-03-02 08:43:36 -0500

    nt4 se crea ejemplos de ciclos y listas

---
+++ ciclofor.py
@@ -0,0 +1,2 @@
+for controlador in range(5,16,2):
+    print(controlador)
---
+++ lista1.py
@@ -0,0 +1,15 @@
+#Lista de datos
+numeros=[1,2,3,4,5]
+#acceder a todos los datos al mismo tiempo
+print(numeros)
+#acceder a un solo elemento de la lista
+print(numeros[0])
+#acceder de forma individual a cada elemento de una lista
+for lista in range(numeros[0],numeros[4],1):
+    print("estoy saltando")
+
+
+for numero in numeros:
+    print("estoy saltando")
+
+
